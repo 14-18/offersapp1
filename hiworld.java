d2e3ea433b4c4965065ab5217ad06e4b6ad61dc3
Jatin Bisht <jatinbisht1990@gmail.com>
2019-07-04 16:48:18 +0000

    added A NEW PRINT STATEMENT

--- hiworld.java
+++ hiworld.java
@@ -1,6 +1,6 @@
 //This is an hello world java program
 public class hiworld{  
         public static void main(String args[]){  
-         System.out.println("Hello Java");  
+         System.out.println("Hello Java");
         }  
     }  
