e1cbf811549cbed2a7cd69c70974a25d6d12ee73
Jatin Bisht <jatinbisht1990@gmail.com>
2019-07-04 16:53:32 +0000

    Added comment line and one print statement

--- hiworld.java
+++ hiworld.java
@@ -1,6 +1,11 @@
 //This is an hello world java program
 public class hiworld{  
         public static void main(String args[]){  
+         // prints "Hello Java"
          System.out.println("Hello Java");
+
+         //prints "Welcome to Git to the terminal window"
+         System.out.println("Welcome to Git");
+
         }  
     }  
