155430670776c1c3bc0f453fd367b74ccc6d2fdc
Jatin Bisht <jatinbisht1990@gmail.com>
2019-07-04 13:32:20 +0000

    Added comment line and created README file

---
+++ README.md
@@ -0,0 +1,6 @@
+This is an offerapp1 code
+This is written in Java
+
+Team members"
+Jatin
+Nitin
--- hiworld.java
+++ hiworld.java
@@ -1,3 +1,4 @@
+//This is an hello world java program
 public class hiworld{  
         public static void main(String args[]){  
          System.out.println("Hello Java");  
