def2eb9dd1c5f0c611704519474aa0dec9e34f44
Jatin Bisht <47514554+14-18@users.noreply.github.com>
2021-12-06 22:34:15 +0530

    Delete hiworld.java

--- hiworld.java
+++
@@ -1,8 +0,0 @@
-public class hiworld{  
-        public static void main(String args[]){  
-
-         System.out.println("Hello Java");  
-//Added as part of ver-1 release
-System.out.println("Welcome to git");
-        }  
-    }  
